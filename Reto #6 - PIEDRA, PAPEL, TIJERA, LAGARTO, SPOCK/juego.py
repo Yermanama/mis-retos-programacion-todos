136c81e65597f2169cdfa0741fb306c8389fafe0
Germán Martínez <martinezdiazgerman@gmail.com>
2023-05-21 15:43:26 +0200

    reto 6 completado

--- Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK/juego.py
+++ Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK/juego.py
@@ -28,9 +28,12 @@ reglas = {
 }
 
 def obtener_eleccion_jugador():
-    eleccion = input("Escribe 5 elecciones para jugar:[piedra, papel, tijera, lagarto, spock]")
-    lista_jugador = eleccion.split("")
-    return lista_jugador
+    while True:
+        eleccion = input("Escribe 5 elecciones para jugar:[piedra, papel, tijera, lagarto, spock]:\n")
+        lista_jugador = eleccion.split(", ")
+        if all(eleccion in reglas.keys() for eleccion in lista_jugador):
+            return lista_jugador
+        print("Las opciones que has proporcionado no son adecuadas")
 
 def obtener_pares():
     lista_elecciones_p1 = obtener_eleccion_jugador()
@@ -38,5 +41,29 @@ def obtener_pares():
     lista_tuplas = list(zip(lista_elecciones_p1, lista_elecciones_p2))
     return lista_tuplas
 
-def obtener_marcador():
-    pass
+def obtener_marcador(lista):
+    counter1 = 0
+    counter2 = 0
+    for tupla in lista:
+        if tupla[1] in reglas[tupla[0]]:
+            print(f"{tupla[0]} VS {tupla[1]}")
+            print(f"{tupla[0]} gana a {tupla[1]}")
+            counter1 += 1
+        else:
+            print(f"{tupla[0]} VS {tupla[1]}")
+            if tupla[1] == tupla[0]:
+                print("Empate, nadie gana puntos")
+            else:
+                print(f"{tupla[0]} gana a {tupla[1]}")
+                counter2 += 1
+        print(f"El marcador actual es Jugador 1: {counter1} VS Jugador 2: {counter2}")
+    if counter1 == counter2:
+        print("Tenemos un empate")
+    elif counter1 > counter2:
+        print("El jugador 1 ha ganado la partida")
+    else:
+        print("El jugador 2 ha ganado la partida")
+
+
+if __name__ == "__main__":
+    obtener_marcador(obtener_pares())
