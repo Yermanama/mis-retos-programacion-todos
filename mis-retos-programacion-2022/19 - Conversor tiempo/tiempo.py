ffc7cb2d09cc3cb361d9305df4be7466a496ffb5
Yermanama <martinezdiazgerman9@gmail.com>
2023-06-11 12:55:42 +0200

    Reto 19 2022

--- mis-retos-programacion-2022/19 - Conversor tiempo/tiempo.py
+++ mis-retos-programacion-2022/19 - Conversor tiempo/tiempo.py
@@ -3,3 +3,12 @@ Crea una función que reciba días, horas, minutos y segundos (como enteros)
 y retorne su resultado en milisegundos.
 """
 
+def convertir_milisegundos(dias: int, horas: int, minutos: int, segundos: int) -> int:
+    dias_milisegundos = dias * 24 * 60 * 60 * 1000
+    horas_milisegundos = horas * 60 * 60 * 1000
+    minutos_milisegundos = minutos * 60 * 1000
+    segundos_milisegundos = segundos * 1000
+    milisegundos: int = dias_milisegundos + horas_milisegundos + minutos_milisegundos + segundos_milisegundos
+    return milisegundos
+
+print(convertir_milisegundos(12,12,12,12))
